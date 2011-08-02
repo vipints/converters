c2c5600a58d4eca5941de2bdf602a0538cd4f536
Vipin T. Sreedharan <vipin.ts@tuebingen.mpg.de>
2011-08-02 15:16:04 +0200

    Program to make a clean formatted FASTA file with genome sequence.

---
+++ Ler_transcriptome/fasta_formatting.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# Formatting FASTA file, it takes a unformatted FASTA file and results a Nice formatted FASTA file.
+
+import re, sys, os 
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.Alphabet import generic_nucleotide
+
+def __main__():
+    
+    try:
+        fasta_raw = sys.argv[1]
+    except: 
+        sys.stderr.write('Access denied for the input FASTA file ! terminating\n')
+        sys.exit(-1)
+    fah = open(fasta_raw, 'rU')
+    for rec in SeqIO.parse(fah, 'fasta'):
+        rna_genome = rec.seq.tomutable()
+        i = 0 
+        for nt in rna_genome:
+            if nt == 'U':
+                rna_genome[i] = 'T'
+            i += 1
+        gseq = SeqRecord(rna_genome, id=rec.id, description='A. thaliana_ribosomal_RNA')
+        print gseq.format("fasta")
+        #break
+    fah.close()
+
+if __name__=="__main__":__main__()

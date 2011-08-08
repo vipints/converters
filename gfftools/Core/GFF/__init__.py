10638f50b776668bc56ba2eba5bf0feb8f83b709
Vipin T. Sreedharan <vipin.ts@tuebingen.mpg.de>
2011-08-08 23:24:51 +0200

    GFF Parser

---
+++ gfftools/Core/GFF/__init__.py
@@ -0,0 +1,4 @@
+"""Top level of GFF parsing providing shortcuts for useful classes.
+"""
+from GFFParser import GFFParser, DiscoGFFParser, GFFExaminer, parse
+from GFFOutput import GFF3Writer, write

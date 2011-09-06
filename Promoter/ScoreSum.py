9428194c6a16ba7a7fe0f8b545cf6234d0386e9d
Vipin T. Sreedharan <vipin.ts@tuebingen.mpg.de>
2011-09-06 20:55:49 +0200

    Memory efficient way to normalize the score in each chromosome.

--- Promoter/ScoreSum.py
+++ Promoter/ScoreSum.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 """
-Normalize the promoter prediction score to [0-1] interval on each chromosome.
+Normalize the promoter prediction score to [0-1] interval on each chromosome. The resulting BED file will be used in pppBenchmark program to evaluate the promoter prediction status.
+
+Usage:
+ScoreSum.py <BED file>
 """
 
 import re, sys 
@@ -8,15 +11,12 @@ from operator import itemgetter
 
 def BEDreader(fname):
     
-    bed_score = dict()
+    bed_score = []
     bfh = open(fname)
     for line in bfh:
         line = line.strip('\n\r').split('\t')
         assert len(line) == 5, '\t'.join(line)
-        if (line[0], line[4]) in bed_score:
-            bed_score[(line[0], line[4])][(int(line[1]), int(line[2]))] = float(line[3])
-        else:
-            bed_score[(line[0], line[4])] = {(int(line[1]), int(line[2])): float(line[3])}
+        bed_score.append(float(line[3]))
     bfh.close()
     return bed_score
 
@@ -25,30 +25,32 @@ def __main__():
     try:
         plus_fname = sys.argv[1]
     except:
-        sys.stderr.write('Access denied for BED file! \n')
+        print 'Incorrect Argument supplied'
+        print __doc__
         sys.exit(-1)
 
     score = BEDreader(plus_fname)
-    for fid, fls in score.items():
-        min_score, max_score = None, None
-        for fp, fs in sorted(fls.items(), key=itemgetter(1), reverse=True):
-            max_score=fs 
+    min_score, max_score = None, None
+    score.sort()
+    for fs in score:
+        min_score=fs
+        if min_score != -42.0:
             break
-        for fp, fs in sorted(fls.items(), key=itemgetter(1)):
-            min_score=fs 
-            if min_score != -42.0: 
-                break
-        for fp, fs in sorted(fls.items()):
-            if fs == -42.0:
-                fs=min_score
-            norm_score=(fs-min_score)/(max_score-min_score)
-            #print norm_score
-            bline = [fid[0],
-                    str(fp[0]),
-                    str(fp[1]),
-                    str(round(norm_score, 4)),
-                    fid[1]
-                    ]
-            print '\t'.join(bline)
+    max_score = score[-1]
+    bfh = open(plus_fname)
+    for line in bfh:
+        line = line.strip('\n\r').split('\t')
+        assert len(line) == 5, '\t'.join(line)
+        if float(line[3]) == -42.0:
+            line[3] = min_score
+        norm_score=(float(line[3])-min_score)/(max_score-min_score)
+        bline = [line[0],
+            line[1],
+            line[2],
+            str(round(norm_score, 4)),
+            line[-1]
+            ]
+        print '\t'.join(bline)
+    bfh.close()
 
 if __name__=="__main__":__main__()

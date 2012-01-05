f9379a31a408a6193d2eb7a6d412c544945af20b
Vipin T. Sreedharan <vipin.ts@tuebingen.mpg.de>
2012-01-05 14:23:53 +0100

    Bug fixing, Zero based coordinates in resulting BED file, thanks to Eduardo M. Reis for catching.

--- gfftools/codebase/gff3_to_bed_converter.py
+++ gfftools/codebase/gff3_to_bed_converter.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# Written (W) 2010-2011 Vipin T Sreedharan, Friedrich Miescher Laboratory of the Max Planck Society
-# Copyright (C) 2010-2011 Max Planck Society
-#
-# Description : Convert genome annotation data in Generic Feature Format version 3 (GFF3) to a 12 column Browser Extensible Data (BED) format. BED format typically represents the transcript models. 
+"""
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
 
+Written (W) 2010-2012 Vipin T Sreedharan 
+Copyright (C) 2010-2012 Friedrich Miescher Laboratory of the Max Planck Society
+
+Description : Convert genome annotation data in Generic Feature Format version 3 (GFF3) 
+to a 12 column Browser Extensible Data (BED) format. BED format typically represents 
+the transcript models. 
+"""
 import re, sys
 from optparse import OptionParser
 
 def stop_err(fmsg):
-    
+    """Error message
+    """
     sys.stderr.write('%s\n' % fmsg)
     sys.exit(-1)
 
 def WriteBED(tinfo, einfo, bed_fh):
-    """BED writter"""
-    
+    """Write content to a BED format
+    """
     for contig_id, feature in tinfo.items():
         for tid, tloc in feature.items():
             if tid in einfo: # get corresponding exon info 
@@ -39,12 +42,12 @@ def WriteBED(tinfo, einfo, bed_fh):
                         exon_cod += str(int(ex_ele[1])-rstart) + ','
                 if exon_len:
                     pline = [str(contig_id),
-                            tloc[0],
+                            str(int(tloc[0])-1),
                             tloc[1],
                             tid,
                             tloc[2],
                             tloc[-1],
-                            tloc[0],
+                            str(int(tloc[0])-1),
                             tloc[1],
                             '0',
                             str(exon_cnt),
@@ -54,17 +57,15 @@ def WriteBED(tinfo, einfo, bed_fh):
     bed_fh.close()
 
 def ParseAnno(gff_fh):
-    """Reading GFF3 file to get feature annotation"""
-
+    """Reading GFF3 file to get feature annotation
+    """
     tinfo, einfo = dict(), dict()
     features = ['transcript', 'scRNA', 'mRNA', 'ncRNA', 'miRNA', 'rRNA', 'snoRNA', 'snRNA', 'tRNA', 'pseudogenic_transcript'] ## TODO: Growing list of RNA species. 
-
     for gff_line in gff_fh:
         gff_line = gff_line.strip('\n\r').split('\t')
         if re.match(r'#', gff_line[0]) or re.match(r'>', gff_line[0]) or len(gff_line) == 1:continue ## not considering commented and FASTA  lines from GFF
         if '' in gff_line:continue ## empty fields in any line ? 
         assert len(gff_line) == 9, '\t'.join(gff_line) ## a valid GFF line contains only 9 tab-delimited fields
-
         if gff_line[2] in features:
             tid = None
             for ele in gff_line[-1].split(';'):
@@ -89,25 +90,23 @@ def ParseAnno(gff_fh):
     return tinfo, einfo
 
 def __main__():
-
     cmd_arg = OptionParser()
     cmd_arg.add_option('', '-q', dest='query_file', help='Data in GFF3 file')
     cmd_arg.add_option('', '-o', dest='result_file', help='BED file name')
-
     options, args = cmd_arg.parse_args()
-    if len(sys.argv) < 2:cmd_arg.print_help();sys.exit(-1)
-
+    if len(sys.argv) < 2:
+        cmd_arg.print_help()
+        sys.exit(-1)
     if options.query_file != None and options.result_file != None: 
         try:
             gff_fh = open(options.query_file, 'rU')
         except Exception, erm:
             stop_err('Error reading query file ' + str(erm))
-        
-        tinfo, einfo = ParseAnno(gff_fh) ## get transcript annotation from GFF3 file 
         try:
             bed_fh = open(options.result_file, 'w')
         except Exception, erm:
             stop_err('Error writing result file ' + str(erm))
+        tinfo, einfo = ParseAnno(gff_fh) ## get transcript annotation from GFF3 file 
         WriteBED(tinfo, einfo, bed_fh) ## BED writter 
 
 if __name__ == "__main__": __main__() 

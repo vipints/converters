a84f6510ff2d7e7d55db23aff1ef13936babd51a
Vipin T. Sreedharan <vipin.ts@tuebingen.mpg.de>
2012-01-14 19:02:47 +0100

    Handling the bug with different source in GFF3

--- gfftools/codebase/gff3_to_gtf_converter.pl
+++ gfftools/codebase/gff3_to_gtf_converter.pl
@@ -5,8 +5,8 @@
 # the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
-# Written (W) 2010-2011 Vipin T Sreedharan, Friedrich Miescher Laboratory of the Max Planck Society
-# Copyright (C) 2010 Max Planck Society
+# Written (W) 2010-2012 Vipin T Sreedharan, Friedrich Miescher Laboratory of the Max Planck Society
+# Copyright (C) 2010-2012 Max Planck Society
 #
 # Description : Convert data in Generic Feature Format Version 3 (GFF3) to Gene Transfer Format (GTF).
  
@@ -16,9 +16,9 @@ use Bio::FeatureIO;
 
 my $usage = q(
 gff3_to_gtf_converter.pl  - Program to convert a valid GFF3 format file to GTF format.
-USAGE: gff3_to_gtf_converter.pl <GFF3 file name> <output file name>
-);
 
+USAGE: gff3_to_gtf_converter.pl in.gff3 out.gtf
+);
 if (scalar(@ARGV) != 2) {
     print $usage;
     exit
@@ -31,11 +31,9 @@ my $inGFF = Bio::FeatureIO->new( '-file' => "$inFile",
 my $outGTF = Bio::FeatureIO->new( '-file' => ">$outFile",
  '-format' => 'GFF',
  '-version' => 2.5);
-
 my ($gene, $feature_type, $exon_exon_cnt, $cds_exon_cnt) = ('', '', 0, 0);
-
 while (my $feature = $inGFF->next_feature() ) {
-    if ($feature->type->name eq 'mRNA' || $feature->type->name eq 'miRNA' || $feature->type->name eq 'ncRNA' || $feature->type->name eq 'rRNA' || $feature->type->name eq 'snoRNA' || $feature->type->name eq 'snRNA' || $feature->type->name eq 'tRNA' || $feature->type->name eq 'misc_RNA' || $feature->type->name eq 'processed_transcript' || $feature->type->name eq 'transcript' || $feature->type->name eq 'scRNA') {
+    if ($feature->type->name eq 'mRNA' || $feature->type->name eq 'miRNA' || $feature->type->name eq 'ncRNA' || $feature->type->name eq 'rRNA' || $feature->type->name eq 'snoRNA' || $feature->type->name eq 'snRNA' || $feature->type->name eq 'tRNA' || $feature->type->name eq 'misc_RNA' || $feature->type->name eq 'processed_transcript' || $feature->type->name eq 'transcript' || $feature->type->name eq 'scRNA' || $feature->type->name eq 'pseudogenic_transcript') {
         my $parent = ($feature->get_Annotations('Parent'))[0]; 
         $gene = $parent->value;
         ($cds_exon_cnt, $exon_exon_cnt) = (1, 1); 
@@ -44,7 +42,6 @@ while (my $feature = $inGFF->next_feature() ) {
     if ($feature->type->name eq 'exon' || $feature->type->name eq 'CDS' ||$feature->type->name eq 'stop_codon' || $feature->type->name eq 'start_codon') {
         my $parent = ($feature->get_Annotations('Parent'))[0];
         my $transcript = $parent->value;
-       
         my ($col_exon_number, $protein_id) = ('', '');
         if ($feature->type->name eq 'exon') {
             $col_exon_number = Bio::Annotation::SimpleValue->new( '-value' => $exon_exon_cnt, '-tagname' => 'exon_number');

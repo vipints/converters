2f9be7d14fb2df18141e3d810c4ed1c77a126eb6
Vipin T. Sreedharan <vipin.ts@tuebingen.mpg.de>
2012-02-12 23:03:36 +0100

    filtering alignments based on the aligned read length.

--- Ler_transcriptome/utils/separate_alignments.py
+++ Ler_transcriptome/utils/separate_alignments.py
@@ -1,55 +1,30 @@
 #!/usr/bin/python 
 """This program used to separate the read alignments according to the feature type.
-Usage:
-samtools view BAM | python separate_alignments.py -f <feature annotation in GFF>
+Usage: python separate_alignments.py in.bam STDOUT SAM file 
 """
 import re, sys 
+import pysam 
 
-def ParseAnno(feat_file):
-    
-    candidates = dict()
-    afh = open(feat_file)
-    for line in afh:
-        line = line.strip('\n\r').split('\t')
-        if re.search(r'^#', line[0]):
-            continue
-        fid = None
-        if line[2] == 'pseudogene':
-            for ele in line[-1].split(';'):
-                if re.search(r'ID=', ele):
-                    fid = re.search(r'ID=(.+)', ele).group(1)
-                    break
-            candidates[fid]=1
-    return candidates
-    afh.close()
-
-def BAMParse(bfh, featdb):
-    
-    PSDfh = open('Psedo.sam', 'w')
-    STRfh = open('Oth.sam', 'w')
-    for line in bfh:
-        line = line.strip('\n\r').split('\t')
-        if line[2] in featdb:
-            PSDfh.write('\t'.join(line)+'\n')
-        else:
-            STRfh.write('\t'.join(line)+'\n')
-    PSDfh.close()
-    STRfh.close()
+def BAMParse(fname):
+    """filter BAM file 
+    """
+    bfh = pysam.Samfile(fname, 'rb') 
+    outsam = pysam.Samfile('-', 'w', template = bfh)
+    for rec in bfh.fetch():
+        if len(rec.seq)==40:
+            if rec.qlen==40:
+                outsam.write(rec)
+        elif len(rec.seq)==42:
+            if rec.qlen==42:
+                outsam.write(rec)
     bfh.close()
+    outsam.close()
 
 if __name__=="__main__":
     try:
-        if sys.argv[1] == '-f':
-            bfh = sys.stdin
-        anno_file = sys.argv[2]
+        fbam = sys.argv[1]
     except:
         print 'Incorrect number of arguments provided:'
         print __doc__
         sys.exit(-1)
-    
-    featdb = ParseAnno(anno_file)
-    for fid in featdb:
-        print fid
-        break
-    BAMParse(bfh, featdb)
-    #print len(featdb)
+    BAMParse(fbam)

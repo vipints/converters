b1784170ea66d66599167b8afd21b93dd18a7986
Vipin T. Sreedharan <vipin.ts@tuebingen.mpg.de>
2012-04-30 17:33:56 +0200

    optimized search in a single iteration for whole chromsome.

--- Ler_transcriptome/fetch_BAM.py
+++ Ler_transcriptome/fetch_BAM.py
@@ -4,12 +4,15 @@ Program to find the number of reads aligned to each TE element region in
 A. thaliana genome. 
 Usage: fetch_BAM.py in.bam in.gff3
 """
+import os 
 import sys, re, pysam 
 
 def get_alignment(bfname):
     """Read BAM file and count the number of alignments for each read.
     """
     aln_cnt=dict()
+    if not os.path.exists(bfname+ ".bai"):
+        pysam.index(bfname)
     bam_read = pysam.Samfile(bfname, 'rb') 
     for rec in bam_read.fetch():
         if rec.qname in aln_cnt:
@@ -19,28 +22,20 @@ def get_alignment(bfname):
     bam_read.close()
     return aln_cnt
 
-def get_annotation(fname):
+def get_annotation(fname, rfname):
     """Parse genome annotation from GFF3 file.
     """
     from Core import GFF
     te_featdb, teg_featdb, ribo_featdb, oth_featdb=dict(), dict(), dict(), dict()
     for cid in ['Chr1', 'Chr2', 'Chr3', 'Chr4', 'Chr5', 'ChrM', 'ChrC']: ## change according to the GFF file TODO According to the file type add chromosome number automatically.
-        print cid
+        sys.stderr.write(cid+".....\n")
         fh = open(fname, 'rU')
         limit_info = dict(gff_id=[cid], gff_source=['TAIR10']) ## change the source TODO According to the file type add source flag automatically 
         for rec in GFF.parse(fh, limit_info=limit_info):
             for each_rec in rec.features:
                 if each_rec.type=='gene':
                     for child in each_rec.sub_features:
-                        if child.type=='rRNA':
-                            if cid in ribo_featdb:
-                                ribo_featdb[cid][(int(each_rec.location._start.position), 
-                                    int(each_rec.location._end.position))]=each_rec.strand
-                            else:
-                                ribo_featdb[cid]={(int(each_rec.location._start.position), 
-                                    int(each_rec.location._end.position)): 
-                                    each_rec.strand}
-                        elif child.type in ['mRNA', 'miRNA', 'ncRNA', 'snoRNA', 'snRNA', 'tRNA']:
+                        if child.type in ['mRNA', 'miRNA', 'ncRNA', 'snoRNA', 'snRNA', 'tRNA']:
                             if cid in oth_featdb:
                                 oth_featdb[cid][(int(each_rec.location._start.position), 
                                     int(each_rec.location._end.position))]=each_rec.strand
@@ -73,15 +68,16 @@ def get_annotation(fname):
                                 int(each_rec.location._end.position)): 
                                 each_rec.strand}
         fh.close()
-    """ribo_featdb['Chr3'][(14199917, 14203578)]=1 ## Unannotated rRNA from A thaliana genome. 
-    ribo_featdb['Chr2'][(5784, 9683)]=1 ## Unannotated rRNA from A thaliana genome. 
-    ribo_featdb['Chr2'][(2821, 3704)]=1 #
-    ribo_featdb['Chr3'][(14196614, 14197675)]=1 #
-    ribo_featdb['Chr3'][(14194052, 14194611)]=1 #
-    ribo_featdb['Chr3'][(14199498, 14199751)]=1 #
-    ribo_featdb['Chr3'][(14195564, 14195739)]=1 #
-    ribo_featdb['ChrM'][(11426, 11883)]=-1 #
-    ribo_featdb['ChrM'][(364594, 365124)]=1 #"""
+    ## add additional rRNA related location from A. thaliana
+    smap={'+' : 1, '-' : -1}
+    fh=open(rfname, 'rU')
+    for line in fh:
+        line=line.strip('\n\r').split(' ')
+        if line[0] in ribo_featdb:
+            ribo_featdb[line[0]][(int(line[1]), int(line[2]))]=smap[line[3]]
+        else:
+            ribo_featdb[line[0]]={(int(line[1]), int(line[2])):smap[line[3]]}
+    fh.close()
     return te_featdb, teg_featdb, ribo_featdb, oth_featdb
 
 def get_region_alignment(bam_fh, ctg_id, start, stop):
@@ -103,14 +99,16 @@ def get_region_alignment(bam_fh, ctg_id, start, stop):
     bamdb = [(fid, finfo) for fid, finfo in samdb.items()]
     return bamdb
 
-def get_ribo_reads(bam_fh, rib_fdb):
+def get_ribo_reads(bfname, rib_fdb):
     """Take the reads from ribosomal RNA region
     """
     ribo_db=dict()
     smap={'+' : 1, '-' : -1}
-    for contig_nb, rib_info in rib_fdb.items():
-        for cod, fstd in rib_info.items(): 
-            for each_align in bam_fh.fetch(contig_nb, cod[0], cod[1]):
+    bam_read = pysam.Samfile(bfname, 'rb') 
+    for contig_nb, rib_info in sorted(rib_fdb.items()):
+        for cod, fstd in sorted(rib_info.items()): 
+            sys.stderr.write(contig_nb+' '+str(cod[0]) +' '+ str(cod[1])+ '...\n')
+            for each_align in bam_read.fetch(contig_nb, cod[0], cod[1]):
                 for cont in each_align.tags:
                     if cont[0]=="XS":
                         orient=cont[1]
@@ -119,6 +117,7 @@ def get_ribo_reads(bam_fh, rib_fdb):
                     ribo_db[each_align.qname].append((fstd, smap[orient]))
                 else:
                     ribo_db[each_align.qname]=[(fstd, smap[orient])]
+    bam_read.close()
     return ribo_db
 
 def process_elements(contig_id, read_db, te_fdb, ribo_read_db, bam_reader, rib_fdb, oth_fdb, teg_fdb, te_marker):
@@ -178,8 +177,8 @@ def process_elements(contig_id, read_db, te_fdb, ribo_read_db, bam_reader, rib_f
 
 def writeSAM(bam_reader, sense_reads, asense_reads, contig_id):
 
-    outsam_sense=pysam.Samfile(contig_id+'_sense.sam', 'w', template=bam_reader)
-    outsam_asense=pysam.Samfile(contig_id+'_antisense.sam', 'w', template=bam_reader)
+    outsam_sense=pysam.Samfile(contig_id+'_S.sam', 'w', template=bam_reader)
+    outsam_asense=pysam.Samfile(contig_id+'_AS.sam', 'w', template=bam_reader)
     for rec in bam_reader.fetch():
         if rec.qname in sense_reads:
             outsam_sense.write(rec)
@@ -213,15 +212,21 @@ if __name__=="__main__":
     try:
         bamfname=sys.argv[1] 
         gffname=sys.argv[2]
-        contig_id=sys.argv[3]
+        add_rRNA=sys.argv[3]
     except:
         print __doc__
         sys.exit(-1)
     read_db=get_alignment(bamfname) # get alignment count for each read
-    te_fdb, teg_fdb, rib_fdb, oth_fdb=get_annotation(gffname) # fetch genome annotation 
-    te_marker=overlap_feature(te_fdb, teg_fdb, rib_fdb, oth_fdb, contig_id)
+    sys.stderr.write(str(len(read_db))+' mapped reads stored in dym db\n')
+    te_fdb, teg_fdb, rib_fdb, oth_fdb=get_annotation(gffname, add_rRNA) # fetch genome annotation 
+    sys.stderr.write('genome annotation stored in dym db\n')
+    ribo_read_db=get_ribo_reads(bamfname, rib_fdb) # get rRNA region reads 
+    sys.stderr.write('rRNA read ids stored in dym db\n')
+    sys.stderr.write(str(len(ribo_read_db))+' reads to rRNA \n')
     bam_reader=pysam.Samfile(bamfname, "rb") # BAM file handle for querying different feature interval
-    ribo_read_db=get_ribo_reads(bam_reader, rib_fdb) # get rRNA region reads 
-    sense_reads, asense_reads=process_elements(contig_id, read_db, te_fdb, ribo_read_db, bam_reader, rib_fdb, oth_fdb, teg_fdb, te_marker)
-    writeSAM(bam_reader, sense_reads, asense_reads, contig_id)
+    for cid in ['Chr1', 'Chr2', 'Chr3', 'Chr4', 'Chr5']: # check for TE's in Chr1..5
+        te_marker=overlap_feature(te_fdb, teg_fdb, rib_fdb, oth_fdb, cid)
+        sys.stderr.write(cid+' feature indexing done\n')
+        sense_reads, asense_reads=process_elements(cid, read_db, te_fdb, ribo_read_db, bam_reader, rib_fdb, oth_fdb, teg_fdb, te_marker)
+        writeSAM(bam_reader, sense_reads, asense_reads, cid)
     bam_reader.close()
